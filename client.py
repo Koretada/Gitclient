7d3f9283452179b1be16e46411ef271cef4ec5e1
Walid <walid.bounhach@grenoble-inp.org>
2023-10-05 15:37:11 +0200

    coucou

--- client.py
+++ client.py
@@ -4,14 +4,14 @@ class GitClient:
     def __init__(self, local_repo_path, remote_repo_url):
         self.local_repo_path = local_repo_path
         self.remote_repo_url = remote_repo_url
-        self.repo = None
-
+        self.repo = git.Repo.init(local_repo_path)
+        """
         if not git.repo.fun.is_git_dir(local_repo_path):
             self.repo = git.Repo.init(local_repo_path)
             self.repo.create_remote('origin', remote_repo_url)
         else:
             self.repo = git.Repo(local_repo_path)
-
+        """
     def clone(self):
         if not self.repo : 
             git.Repo.clone_from(self.remote_repo_url, self.local_repo_path)
@@ -28,4 +28,6 @@ class GitClient:
         self.repo.index.commit(message)
 
 if __name__ == "__main__":
-    client = GitClient('/home/debian/project', 'git@192.168.1.10:/opt/git/project.git')    
+    client = GitClient('/home/koretada/project', 'git@192.168.1.10:/opt/git/project.git')    
+    client.commit("toto")
+    client.push()
